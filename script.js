8cffbae3c46721e711e51e4988a342ae16463c3f
praveenWebDesigner360 <m.praveenkumar.github@gmail.com>
2023-05-06 07:19:56 +0530

    Implementing the game logic

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 'use strict';
 
-// const message = document.querySelector('.message').textContent;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
+// {------ Start Check btn ------}
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⭕ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🥳 Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = '💥 You lost the game';
+    }
   }
 });
+// {------ End Check btn --------}
